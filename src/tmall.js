ddd33f958d6dc217fb4cff38e0402d5ba084c8cd
Huajian Mao <huajianmao@gmail.com>
2017-02-14 08:16:25 +0800

    Add get informations from localStorage.

--- src/tmall.js
+++ src/tmall.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  save the browsered tmall sale list page!
 // @author       Huajian Mao
-// @match        https://shengmengtu.tmall.com/search.htm?spm=a1z10.1-b.w5001-13303190287.9.TR32f1&scene=taobao_shop
+// @match        https://shengmengtu.tmall.com/search.htm?spm=*&scene=taobao_shop
 // @grant        none
 // ==/UserScript==
 
@@ -88,8 +88,90 @@
 
         localStorage.setItem('sales', JSON.stringify(sales));
     }
+    function getSaleNumsOfId(id) {
+        var sales = localStorage.getItem('sales');
+
+        if (sales === null || sales === undefined) {
+            sales = {};
+        } else {
+            sales = JSON.parse(sales);
+        }
+
+        var thisSale = sales[id];
+        if (thisSale === null || thisSale === undefined) {
+            console.log("Item not found!");
+        } else {
+            var saleNums = thisSale.num;
+            if (saleNums === null || saleNums === undefined) {
+                console.log("Item has no sale number history!");
+            } else {
+                console.log(thisSale.title);
+                for (var saleNum of saleNums) {
+                    var dateStr = unixtime2formated(saleNum.date);
+                    console.log("[" + dateStr + "] " + saleNum.num);
+                }
+                console.log("========================");
+            }
+        }
+    }
+
+    function getTotalSaleNums() {
+        var sales = getSalesFromLocalStorage();
+        var total = sales.total;
+        if (total === null || total === undefined || total.length === 0) {
+            console.log("No total data.");
+        } else {
+            var totalObj;
+            var dateStr;
+            if (total.length >= 2) {
+                totalObj = total[total.length - 2];
+                dateStr = unixtime2formated(totalObj.date);
+                console.log("[" + dateStr + "] " + totalObj.num + " (约" + totalObj.money + "元)");
+            }
+            if (total.length >= 1) {
+                totalObj = total[total.length - 1];
+                dateStr = unixtime2formated(totalObj.date);
+                console.log("[" + dateStr + "] " + totalObj.num + " (约" + totalObj.money + "元)");
+            }
+        }
+    }
+
+
+    function getUpdatedSaleNums() {
+        var updatedSaleIds = new Set();
+        var sales = getSalesFromLocalStorage();
+        for (var id of Object.keys(sales)) {
+            var thisSale = sales[id];
+            var saleNums = thisSale.num;
+            if (saleNums === null || saleNums === undefined || saleNums.length === 0) {
+                continue;
+            } else if (saleNums.length === 1) {
+                updatedSaleIds.add(id);
+                continue;
+            } else if (saleNums.length >= 2) {
+                var saleLength = saleNums.length;
+                if (saleNums[saleLength - 1].num !== saleNums[saleLength - 2].num) {
+                    updatedSaleIds.add(id);
+                    continue;
+                }
+            }
+        }
+
+        for (var thisId of updatedSaleIds) {
+            getSaleNumsOfId(thisId);
+        }
+    }
 
     document.onreadystatechange = function(event) {
         updateSalesOnTmall();
+
+
+        getUpdatedSaleNums();
+
+        for (var id of ["542909419938", "527186950590", "527396610383", "539987603681"]) {
+            getSaleNumsOfId(id);
+        }
+
+        getTotalSaleNums();
     };
 })();
